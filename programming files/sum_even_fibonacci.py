4b3b447e815166d819079222cc961f429f35b5f8
sgmalik <112639907+sgmalik@users.noreply.github.com>
2024-04-11 10:34:34 -0400

    Update sum_even_fibonacci.py

--- programming files/sum_even_fibonacci.py
+++ programming files/sum_even_fibonacci.py
@@ -1,8 +1,9 @@
+#Bugs introduced: SM
 a, b = 0, 1
 sum_even = 0
-while b < 4000000:
-    if b % 2 == 0:
-        sum_even += b
+while b < 4000000 #removed :
+    if b % 2 == 0 #removed :
+        sum_even ++= b #added +
     a, b = b, a+b
 
 print(sum_even)
