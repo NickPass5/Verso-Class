53bbcf341722dcaaec4bf7314494080eddebe65d
NickPass5 <112531432+NickPass5@users.noreply.github.com>
2024-04-11 10:27:11 -0400

    Added comment to even_odd_site.java

--- programming files/even_odd_site.java
+++ programming files/even_odd_site.java
@@ -7,6 +7,7 @@ public class even_odd_site {
         BufferedWriter file = new BufferedWriter(new FileWriter("numbers.html"));
         file.write("<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n");
         file.write("<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n");
+        // For loop that writes to a table in Java
         for (int i = 1; i <= 50; i++) {
             if (i % 2 == 0) {
                 file.write("<tr><td>" + i + "</td><td></td></tr>\n");
