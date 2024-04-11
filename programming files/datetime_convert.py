b7dae7f264b4c9f40937a82c752de1f05ed6541f
NickPass5 <112531432+NickPass5@users.noreply.github.com>
2024-04-11 10:36:10 -0400

    Update datetime_convert.py

--- programming files/datetime_convert.py
+++ programming files/datetime_convert.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 
+# bugs introduced: NP
 date_str = "2022-03-17 10:45:30" #start time
-date_obj = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S') #datetime built in function
+date_obj = datetim.strptime(date_str, '%Y-%m-%d %H:%M:%S') #datetime built in function
 formatted_date = date_obj.strftime('%m/%d/%Y %H:%M:%S') #datetime built in function
 
 print(formatted_date)
