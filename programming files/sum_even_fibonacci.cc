145d4ae93648d42cc59f04ad428e8f2ad505bfd5
jmathies1 <143812020+jmathies1@users.noreply.github.com>
2024-04-11 10:42:52 -0400

    Introduced bugs to sum_even_fibonacci.cc

--- programming files/sum_even_fibonacci.cc
+++ programming files/sum_even_fibonacci.cc
@@ -1,3 +1,5 @@
+// bugs introduced: JM
+
 #include <iostream>
 
 int main()
@@ -5,7 +7,7 @@ int main()
     int a = 0, b = 1;
     int sum_even = 0;
     while (b < 4000000) {
-        if (b % 2 == 0) {
+        if (b / 2 != 0) {
             sum_even += b;
         }
         int temp = b;
